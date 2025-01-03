98445f690f23749c0aa6e10d31b8e7707c287607
NRSKhouw <nickr.setiawan@gmail.com>
2025-01-03 02:09:01 -0500

    format output

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 def emotion_detector(text_to_analyse):
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
@@ -6,5 +7,10 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
 
     response = requests.post(url, json = myobj, headers = headers)
+    formatted_response = json.loads(response.text)
+    emotion_score = formatted_response["emotionPredictions"][0]["emotion"]
+    dominant_emotion_key = max(emotion_score, key = emotion_score.get)
 
-    return response.text
+    emotion_score['dominant_emotion'] = dominant_emotion_key
+
+    return emotion_score
