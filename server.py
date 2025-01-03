daf0cdb9f0348322e4ca629710266eb8d10a9f18
NRSKhouw <nickr.setiawan@gmail.com>
2025-01-03 03:04:15 -0500

    linting

--- server.py
+++ server.py
@@ -12,21 +12,23 @@ app = Flask("Emotion Detector")
 
 @app.route("/emotionDetector")
 def sent_analyzer():
+    ''' Emotion Detector using AI
+    '''
     # Retrieve the text to analyze from the request arguments
     text_to_analyze = request.args.get('textToAnalyze')
 
     # Pass the text to the sentiment_analyzer function and store the response
     response = emotion_detector(text_to_analyze)
 
-    emotion_values = ", ".join(f"'{key}': {value}" for key, value in response.items() if key != "dominant_emotion")
+    emotion_values = ", ".join(f"'{key}': {value}" for key,
+        value in response.items() if key != "dominant_emotion")
     output = (
                 f"For the given statement, the system response is {emotion_values}. "
                 f"The dominant emotion is <b>{response['dominant_emotion']}</b>."
                 )
     if response['anger'] is None:
         return "<b>Invalid text! Please try again!</b>"
-    else:
-        return output
+    return output
 
 @app.route("/")
 def render_index_page():
@@ -36,6 +38,4 @@ def render_index_page():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    ''' This functions executes the flask app and deploys it on localhost:5000
-    '''
     app.run(host="0.0.0.0", port=5000)
